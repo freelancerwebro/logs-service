934836084f04e6362c7a3d388a8f393176ea070e
Sorin Marian <marian.sorin@yahoo.com>
2024-03-10 13:23:03 +0200

    Add small improvements

--- src/Command/SaveLogsCommand.php
+++ src/Command/SaveLogsCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:save-logs',
@@ -29,45 +28,13 @@ final class SaveLogsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // $io = new SymfonyStyle($input, $output);
-        //        $arg1 = $input->getArgument('filepath');
-        //
-        //        if ($arg1) {
-        //            $io->note(sprintf('You passed an argument: %s', $arg1));
-        //        }
-        //
-        //        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        try {
+            $this->saveLogService->save();
+        } catch (\Throwable $throwable) {
+            $output->write($throwable->getMessage());
 
-        // parse file
-
-        // save to DB
-
-        // die($this->logFilePath . ', ' . $this->fileRegex);
-
-        //        $handle = fopen($this->logFilePath, "r");
-        //
-        //        if ($handle) {
-        //            while (($line = fgets($handle)) !== false) {
-        //                $match = @preg_match($this->fileRegex, $line, $matches);
-        //
-        //                $res = $this->parseResult($matches);
-        //
-        //                print_r($res);
-        //
-        //                sleep(5);
-        //            }x
-        //        }
-
-        //        $parser = new SimpleParser($this->fileRegex);
-        //
-        //        foreach (new LogIterator($this->logFilePath, $parser) as $data) {
-        //            var_export($data);
-        //            echo "\n";
-        //
-        //            sleep(5);
-        //        }
-
-        $this->saveLogService->save();
+            return Command::FAILURE;
+        }
 
         return Command::SUCCESS;
     }
