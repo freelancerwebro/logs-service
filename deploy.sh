4b6712c25314ee1ae733648e91cdf0b7cff02b96
Sorin Marian <marian.sorin@yahoo.com>
2025-03-09 11:58:22 +0200

    Fix project setup

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,20 @@ echo 'Prepare config files...'
 cp phpunit.xml.dist phpunit.xml
 cp phpstan.dist.neon phpstan.neon
 cp behat.yml.dist behat.yml
+cp .env.example .env
 
 echo 'Building containers...'
-docker-compose up -d
+docker-compose up -d --build
 
 echo 'Installing composer...'
-docker exec -it php composer install --prefer-dist --no-progress --no-interaction
+docker exec -it logs-service-php composer install --prefer-dist --no-progress --no-interaction
 
 echo 'Creating database...'
-docker exec -it php php bin/console doctrine:database:create --if-not-exists --no-interaction
+docker exec -it logs-service-php php bin/console doctrine:database:create --if-not-exists --no-interaction
 
 echo 'Running migrations...'
-docker exec -it php php bin/console doctrine:migrations:migrate --no-interaction
-docker exec -it php php bin/console cache:clear --no-interaction
+docker exec -it logs-service-php php bin/console doctrine:migrations:migrate --no-interaction
+docker exec -it logs-service-php php bin/console cache:clear --no-interaction
 
+echo 'Generate swagger documentation...'
+docker exec -it logs-service-php php bin/console api:openapi:export --format=json > swagger.json
