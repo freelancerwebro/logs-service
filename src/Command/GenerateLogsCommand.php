b5626362fd2596c34d0d284af1ad180ce3d15d83
Sorin Marian <marian.sorin@yahoo.com>
2025-03-14 23:48:07 +0200

    Refactor logs generation command

--- src/Command/GenerateLogsCommand.php
+++ src/Command/GenerateLogsCommand.php
@@ -4,52 +4,82 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Factory\LogFactoryInterface;
-use App\Repository\LogRepositoryInterface;
+use Exception;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\Attribute\When;
+use Throwable;
 
 #[When('dev')]
 #[AsCommand(
     name: 'app:generate-logs',
-    description: 'This is a test command to be used in DEV only. This purpose is just to populate the DB.',
+    description: 'This is a test command to be used in DEV only. This purpose is to generate aggregated logs.',
 )]
 class GenerateLogsCommand extends Command
 {
-    public function __construct(
-        private readonly LogRepositoryInterface $logRepository,
-        private readonly LogFactoryInterface $logFactory,
-    ) {
-        parent::__construct();
+    const LOG_FORMAT = '%s - - [%s] "%s %s HTTP/1.1" %d';
+    const SERVICE_NAMES = ['USER-SERVICE', 'AUTH-SERVICE', 'NOTIFICATION-SERVICE', 'INVOICE-SERVICE', 'PAYMENT-SERVICE'];
+    const METHODS = ['GET', 'POST', 'PUT', 'DELETE'];
+    const CODES = [200, 201, 200, 201, 200, 201, 200, 201, 200, 201, 200, 201, 204, 400, 404, 500, 502, 503];
+    const ENDPOINTS = ['/auth', '/user', '/user/1', '/notify', '/invoice/2', '/payment/1/status'];
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Process a chunk of the log file')
+            ->addArgument('filePath', InputArgument::REQUIRED, 'Path to the log file')
+            ->addArgument('generateRowsNo', InputArgument::OPTIONAL, 'Numbers of rows to be generated', 100000);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $serviceNames = ['USER-SERVICE', 'AUTH-SERVICE', 'NOTIFICATION-SERVICE', 'INVOICE-SERVICE', 'PAYMENT-SERVICE'];
-        $methods = ['GET', 'POST', 'PUT', 'DELETE'];
-        $codes = [200, 201, 200, 201, 200, 201, 200, 201, 200, 201, 200, 201, 204, 400, 404, 500, 502, 503];
-        $endpoints = ['/auth', '/user', '/user/1', '/notify', '/invoice/2', '/payment/1/status'];
+        $filePath = $input->getArgument('filePath');
+        $generateRowsNo = $input->getArgument('generateRowsNo');
 
-        for ($i = 0; $i <= 100000; ++$i) {
-            $serviceName = $serviceNames[array_rand($serviceNames)];
-            $method = $methods[array_rand($methods)];
-            $code = $codes[array_rand($codes)];
-            $endpoint = $endpoints[array_rand($endpoints)];
+        try {
+            for ($i = 0; $i <= $generateRowsNo; ++$i) {
+                $serviceName = self::SERVICE_NAMES[array_rand(self::SERVICE_NAMES)];
+                $method = self::METHODS[array_rand(self::METHODS)];
+                $code = self::CODES[array_rand(self::CODES)];
+                $endpoint = self::ENDPOINTS[array_rand(self::ENDPOINTS)];
 
-            $entity = $this->logFactory->create([
-                'serviceName' => $serviceName,
-                'method' => $method,
-                'statusCode' => $code,
-                'endpoint' => $endpoint,
-                'created' => '2024-03-11 12:33:33',
-                ]);
+                $logEntry = sprintf(self::LOG_FORMAT, $serviceName, date('d/M/Y:H:i:s O'), $method, $endpoint, $code) . "\n";
+                $this->writeIntoFile($logEntry, $filePath);
+            }
 
-            $this->logRepository->save($entity);
+            $output->write('Logs generated successfully');
+        } catch (Throwable $throwable) {
+            $output->write($throwable->getMessage());
+
+            return Command::FAILURE;
         }
 
-        return 0;
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function writeIntoFile(string $logEntry, string $filePath): void
+    {
+        if (!is_writable($filePath) && !is_writable(dirname($filePath))) {
+            throw new Exception("Error: Cannot write to log file.");
+        }
+
+        if (empty($logEntry)) {
+            throw new Exception("Error: Log entry is empty.");
+        }
+
+        $file = fopen($filePath, 'a');
+
+        if (!$file) {
+            throw new Exception("Error: Unable to write to the log file.");
+        }
+
+        fwrite($file, $logEntry);
+        fclose($file);
     }
 }
